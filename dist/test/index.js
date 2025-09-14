61b973414ee0fa9882a2f4a622923a41b9dfadba
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2025-09-14 11:01:20 -0500

    chore: remove TypeScript package and update package-lock.json
    - Deleted TypeScript package from node_modules.
    - Updated package-lock.json to reflect version changes and added new dependencies.
    - Modified test/index.ts to improve code formatting and update charge amounts.

--- dist/test/index.js
+++ dist/test/index.js
@@ -5,42 +5,31 @@ const src_1 = require("../src");
 const charges = [
     {
         order: 1,
-        amount: 0.05,
+        amount: 0.1,
         type: src_1.ChargeTypeEnum.DISCOUNTS,
         application: src_1.ChargeApplicationEnum.PERCENTAGE,
     },
-    {
-        order: 2,
-        amount: 20,
-        type: src_1.ChargeTypeEnum.DISCOUNTS,
-        application: src_1.ChargeApplicationEnum.QUANTITY,
-    },
-    {
-        order: 3,
-        amount: 15,
-        type: src_1.ChargeTypeEnum.SURCHARGES,
-        application: src_1.ChargeApplicationEnum.QUANTITY,
-    },
-    {
-        order: 4,
-        amount: 0.03,
-        type: src_1.ChargeTypeEnum.SURCHARGES,
-        application: src_1.ChargeApplicationEnum.PERCENTAGE,
-    }
 ];
 const concepts = [
     {
         id: 1,
         quantity: 1,
-        basePrice: 120.00,
-        name: 'Mensualidad de zumba - junio',
-        charges
-    }
+        basePrice: 1150,
+        name: "Mensualidad de zumba - junio",
+        charges,
+    },
+    {
+        id: 1,
+        quantity: 1,
+        basePrice: 1150,
+        name: "Mensualidad de zumba - junio",
+        charges,
+    },
 ];
 const traditional = (0, src_1.calculateInvoice)({
     concepts,
     fountType: src_1.FountTypeEnum.DISCOUNT_ON_DISCOUNT,
-    ivaPercentage: src_1.TaxPercentageEnum.T0
+    ivaPercentage: src_1.TaxPercentageEnum.T0,
 });
 traditional.concepts.forEach((value) => {
     var _a, _b, _c, _d, _e, _f;
@@ -51,12 +40,12 @@ traditional.concepts.forEach((value) => {
     console.log(`Cargo                  $ `, (_d = value === null || value === void 0 ? void 0 : value.chargeWithIVA) === null || _d === void 0 ? void 0 : _d.toFixed(6).toString());
     console.log(`Descuento              $ `, (_e = value === null || value === void 0 ? void 0 : value.discountWithIVA) === null || _e === void 0 ? void 0 : _e.toFixed(6).toString());
     console.log(`Total                  $ `, (_f = value === null || value === void 0 ? void 0 : value.amountWithCharges) === null || _f === void 0 ? void 0 : _f.toFixed(6).toString());
-    console.log('\n');
+    console.log("\n");
 });
-console.log('Impuestos \n \n');
+console.log("Impuestos \n \n");
 console.log(`Base de impuestos      $ `, (_a = traditional.baseTax) === null || _a === void 0 ? void 0 : _a.toFixed(6).toString());
 console.log(`Impuesto               $ `, (_b = traditional.tax) === null || _b === void 0 ? void 0 : _b.toFixed(6).toString());
-console.log('Comprobante \n \n');
+console.log("Comprobante \n \n");
 console.log(`Importe                $ `, (_c = traditional.amount) === null || _c === void 0 ? void 0 : _c.toFixed(6).toString());
 console.log(`Descuento              $ `, (_d = traditional.discount) === null || _d === void 0 ? void 0 : _d.toFixed(6).toString());
 console.log(`Impuesto               $ `, (_e = traditional.tax) === null || _e === void 0 ? void 0 : _e.toFixed(6).toString());
--- test/index.ts
+++ test/index.ts
@@ -1,80 +1,103 @@
 import {
-    calculateInvoice,
-    Charge,
-    ChargeApplicationEnum,
-    ChargeTypeEnum,
-    Concept,
-    FountTypeEnum,
-    TaxPercentageEnum,
-} from '../src';
+  calculateInvoice,
+  Charge,
+  ChargeApplicationEnum,
+  ChargeTypeEnum,
+  Concept,
+  FountTypeEnum,
+  TaxPercentageEnum,
+} from "../src";
 
 const charges: Charge[] = [
-    {
-        order: 1,
-        amount: 0.05,
-        type: ChargeTypeEnum.DISCOUNTS,
-        application: ChargeApplicationEnum.PERCENTAGE,
-    },
-    {
-        order: 2,
-        amount: 20,
-        type: ChargeTypeEnum.DISCOUNTS,
-        application: ChargeApplicationEnum.QUANTITY,
-    },
-    {
-        order: 3,
-        amount: 15,
-        type: ChargeTypeEnum.SURCHARGES,
-        application: ChargeApplicationEnum.QUANTITY,
-    },
-    {
-        order: 4,
-        amount: 0.03,
-        type: ChargeTypeEnum.SURCHARGES,
-        application: ChargeApplicationEnum.PERCENTAGE,
-    }
-]
+  {
+    order: 1,
+    amount: 0.1,
+    type: ChargeTypeEnum.DISCOUNTS,
+    application: ChargeApplicationEnum.PERCENTAGE,
+  },
+];
 
 const concepts: Concept[] = [
-    {
-        id: 1,
-        quantity: 1,
-        basePrice: 120.00,
-        name: 'Mensualidad de zumba - junio',
-        charges
-    }
+  {
+    id: 1,
+    quantity: 1,
+    basePrice: 1150,
+    name: "Mensualidad de zumba - junio",
+    charges,
+  },
+  {
+    id: 1,
+    quantity: 1,
+    basePrice: 1150,
+    name: "Mensualidad de zumba - junio",
+    charges,
+  },
 ];
 
 const traditional = calculateInvoice({
-    concepts,
-    fountType: FountTypeEnum.DISCOUNT_ON_DISCOUNT,
-    ivaPercentage: TaxPercentageEnum.T0
+  concepts,
+  fountType: FountTypeEnum.DISCOUNT_ON_DISCOUNT,
+  ivaPercentage: TaxPercentageEnum.T0,
 });
 
-
 traditional.concepts.forEach((value) => {
-    console.log(`Producto - ${value.name}`);
+  console.log(`Producto - ${value.name}`);
 
-    console.log(`Cantidad               $ `, value?.quantity?.toFixed(6).toString())
+  console.log(
+    `Cantidad               $ `,
+    value?.quantity?.toFixed(6).toString()
+  );
 
+  console.log(
+    `Precio unitario        $ `,
+    value?.basePrice?.toFixed(6).toString()
+  );
+  console.log(
+    `Importe                $ `,
+    value?.amountWithoutCharges?.toFixed(6).toString()
+  );
+  console.log(
+    `Cargo                  $ `,
+    value?.chargeWithIVA?.toFixed(6).toString()
+  );
+  console.log(
+    `Descuento              $ `,
+    value?.discountWithIVA?.toFixed(6).toString()
+  );
 
-    console.log(`Precio unitario        $ `, value?.basePrice?.toFixed(6).toString())
-    console.log(`Importe                $ `, value?.amountWithoutCharges?.toFixed(6).toString())
-    console.log(`Cargo                  $ `, value?.chargeWithIVA?.toFixed(6).toString())
-    console.log(`Descuento              $ `, value?.discountWithIVA?.toFixed(6).toString())
+  console.log(
+    `Total                  $ `,
+    value?.amountWithCharges?.toFixed(6).toString()
+  );
 
-    console.log(`Total                  $ `, value?.amountWithCharges?.toFixed(6).toString())
+  console.log("\n");
+});
 
-    console.log('\n')
-})
+console.log("Impuestos \n \n");
 
-console.log('Impuestos \n \n')
+console.log(
+  `Base de impuestos      $ `,
+  traditional.baseTax?.toFixed(6).toString()
+);
+console.log(
+  `Impuesto               $ `,
+  traditional.tax?.toFixed(6).toString()
+);
 
-console.log(`Base de impuestos      $ `, traditional.baseTax?.toFixed(6).toString())
-console.log(`Impuesto               $ `, traditional.tax?.toFixed(6).toString())
-
-console.log('Comprobante \n \n')
-console.log(`Importe                $ `, traditional.amount?.toFixed(6).toString())
-console.log(`Descuento              $ `, traditional.discount?.toFixed(6).toString())
-console.log(`Impuesto               $ `, traditional.tax?.toFixed(6).toString())
-console.log(`Total                  $ `, traditional.total?.toFixed(6).toString())
+console.log("Comprobante \n \n");
+console.log(
+  `Importe                $ `,
+  traditional.amount?.toFixed(6).toString()
+);
+console.log(
+  `Descuento              $ `,
+  traditional.discount?.toFixed(6).toString()
+);
+console.log(
+  `Impuesto               $ `,
+  traditional.tax?.toFixed(6).toString()
+);
+console.log(
+  `Total                  $ `,
+  traditional.total?.toFixed(6).toString()
+);
